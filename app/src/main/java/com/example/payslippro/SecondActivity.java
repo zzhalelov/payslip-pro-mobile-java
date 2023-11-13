9103f2a634c1c7c460fde742d63cd4c32ed29d8a
Zhastilek Zhalelov <zzhalelov@gmail.com>
2023-11-13 23:23:03 +0600

    Add radio group

--- app/src/main/java/com/example/payslippro/SecondActivity.java
+++ app/src/main/java/com/example/payslippro/SecondActivity.java
@@ -6,13 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
     private EditText editTextGross;
     private TextView textViewResult;
-    private RadioButton radioButton1;
-    private RadioButton radioButton2;
+
+    RadioButton radioButtonWithDeduction;
+    RadioButton radioButtonWithoutDeduction;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +23,29 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
         editTextGross = findViewById(R.id.editTextGross);
         textViewResult = findViewById(R.id.textViewResult);
-        radioButton1 = findViewById(R.id.radioButtonWithDeduction);
-        radioButton2 = findViewById(R.id.radioButtonWithoutDeduction);
+
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        radioButtonWithDeduction = findViewById(R.id.radioButtonWithDeduction);
+        radioButtonWithoutDeduction = findViewById(R.id.radioButtonWithoutDeduction);
+        radioGroup.clearCheck();
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.radioButtonWithDeduction){
+                    textViewResult.setText("С вычетом");
+                } else if (checkedId == R.id.radioButtonWithoutDeduction) {
+                    textViewResult.setText("Без вычета");
+                }
+            }
+        });
     }
 
     public void clickButton(View view) {
-            String grossStr = editTextGross.getText().toString();
-            int gross = Integer.parseInt(grossStr);
-            FromGrossToNetWithDeduction fromGrossToNetWithDeduction = new FromGrossToNetWithDeduction();
-            int result = fromGrossToNetWithDeduction.calculateFromGrossToNet(gross);
-            textViewResult.setText(result + "");
+        String grossStr = editTextGross.getText().toString();
+        int gross = Integer.parseInt(grossStr);
+        FromGrossToNetWithDeduction fromGrossToNetWithDeduction = new FromGrossToNetWithDeduction();
+        int result = fromGrossToNetWithDeduction.calculateFromGrossToNet(gross);
+        textViewResult.setText(result + "");
     }
 }
