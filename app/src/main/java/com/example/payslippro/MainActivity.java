2acc998a88ec713a5c26f1a5bba2ecb9daa37529
Zhastilek Zhalelov <zzhalelov@gmail.com>
2023-11-24 21:22:58 +0600

    Modify activity_main.xml, MainActivity.java, strings.xml

--- app/src/main/java/com/example/payslippro/MainActivity.java
+++ app/src/main/java/com/example/payslippro/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.payslippro;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickFirst(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickSecond(View view) {
+        Intent intent = new Intent(this, ThirdActivity.class);
+        startActivity(intent);
     }
 }
