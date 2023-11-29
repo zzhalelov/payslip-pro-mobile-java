f9d6c33d8b2044040142dc8a0522d22c372d129a
Zhastilek Zhalelov <zzhalelov@gmail.com>
2023-11-29 23:31:44 +0600

    Fix the formulas in FromNetToGross classes

--- app/src/main/java/com/example/payslippro/FromNetToGrossWithDeduction.java
+++ app/src/main/java/com/example/payslippro/FromNetToGrossWithDeduction.java
@@ -24,17 +24,13 @@ public class FromNetToGrossWithDeduction extends FromNetToGross {
 
     @Override
     public int calculateOpv(int gross) {
-        if (gross < 700000) {
+        if (gross < 3500000) {
             double opv = gross * 0.1;
             return (int) opv;
-        } else if (gross >= 700000 & gross < 3500000) {
-            double opv = gross * 0.1;
-            return (int) opv;
-        } else if (gross > 3500000) {
+        } else {
             double opv = 350000;
             return (int) opv;
         }
-        return 0;
     }
 
     @Override
@@ -42,24 +38,28 @@ public class FromNetToGrossWithDeduction extends FromNetToGross {
         if (gross < 700000) {
             double vosms = gross * 0.02;
             return (int) vosms;
-        } else if (gross >= 700000 & gross < 3500000) {
-            double vosms = 14000;
-            return (int) vosms;
-        } else if (gross > 3500000) {
+        } else {
             double vosms = 14000;
             return (int) vosms;
         }
-        return 0;
     }
 
     @Override
     public int calculateIpn(int gross) {
-        if (gross < 700000) {
+        if (gross < 54900) {
+            double ipn = 0;
+            return (int) ipn;
+        } else if (gross < 86250) {
+            double opv = gross * 0.1;
+            double vosms = gross * 0.02;
+            double ipn = ((gross - opv - vosms - 48300) * 0.1) * 0.1;
+            return (int) ipn;
+        } else if (gross < 700000) {
             double opv = gross * 0.1;
             double vosms = gross * 0.02;
             double ipn = (gross - opv - vosms - 48300) * 0.1;
             return (int) ipn;
-        } else if (gross >= 700000 & gross < 3500000) {
+        } else if (gross < 3500000) {
             double opv = gross * 0.1;
             double vosms = 14000;
             double ipn = (gross - opv - vosms - 48300) * 0.1;
--- app/src/main/java/com/example/payslippro/FromNetToGrossWithoutDeduction.java
+++ app/src/main/java/com/example/payslippro/FromNetToGrossWithoutDeduction.java
@@ -21,17 +21,13 @@ public class FromNetToGrossWithoutDeduction extends FromNetToGross {
 
     @Override
     public int calculateOpv(int gross) {
-        if (gross < 700000) {
+        if (gross < 3500000) {
             double opv = gross * 0.1;
             return (int) opv;
-        } else if (gross >= 700000 & gross < 3500000) {
-            double opv = gross * 0.1;
-            return (int) opv;
-        } else if (gross > 3500000) {
+        } else {
             double opv = 350000;
             return (int) opv;
         }
-        return 0;
     }
 
     @Override
@@ -39,24 +35,25 @@ public class FromNetToGrossWithoutDeduction extends FromNetToGross {
         if (gross < 700000) {
             double vosms = gross * 0.02;
             return (int) vosms;
-        } else if (gross >= 700000 & gross < 3500000) {
-            double vosms = 14000;
-            return (int) vosms;
-        } else if (gross > 3500000) {
+        } else {
             double vosms = 14000;
             return (int) vosms;
         }
-        return 0;
     }
 
     @Override
     public int calculateIpn(int gross) {
-        if (gross < 700000) {
+        if (gross < 86250) {
+            double opv = gross * 0.1;
+            double vosms = gross * 0.02;
+            double ipn = ((gross - opv - vosms) * 0.1) * 0.1;
+            return (int) ipn;
+        } else if (gross < 700000) {
             double opv = gross * 0.1;
             double vosms = gross * 0.02;
             double ipn = (gross - opv - vosms) * 0.1;
             return (int) ipn;
-        } else if (gross >= 700000 & gross < 3500000) {
+        } else if (gross < 3500000) {
             double opv = gross * 0.1;
             double vosms = 14000;
             double ipn = (gross - opv - vosms) * 0.1;
