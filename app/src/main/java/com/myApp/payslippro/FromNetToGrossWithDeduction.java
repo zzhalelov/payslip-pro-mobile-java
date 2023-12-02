86d141cf5efec98bab6aed2775f2ea48b1e35a9f
Zhastilek Zhalelov <zzhalelov@gmail.com>
2023-12-02 23:57:19 +0600

    Fix a bug in FromNetToGrossWithDeduction.java

--- app/src/main/java/com/myApp/payslippro/FromNetToGrossWithDeduction.java
+++ app/src/main/java/com/myApp/payslippro/FromNetToGrossWithDeduction.java
@@ -5,19 +5,14 @@ public class FromNetToGrossWithDeduction extends FromNetToGross {
     public int calculateFromNetToGross(int net) {
         if (net <= 48300) {
             double gross = net / 0.88;
-            return (int) gross;
         } else if (net > 48300 & net <= 75624) {
             double gross = (net - 483) / 0.8712;
-            return (int) gross;
         } else if (net > 75624 & net <= 559230) {
             double gross = (net - 4830) / 0.792;
-            return (int) gross;
         } else if ((net > 559230 & net <= 2827230)) {
             double gross = (net + 7770) / 0.81;
-            return (int) gross;
         } else if (net > 2827230) {
             double gross = (net + 322770) / 0.9;
-            return (int) gross;
         }
         return 0;
     }
